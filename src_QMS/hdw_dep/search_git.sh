e566a41ae7089e2e7cd6efd53e226384a9b460b2
Alex Pastoriza <apast005@gmail.com>
2020-10-14 02:18:05 -0400

    Finished basic script to automate dependency downloads required for any cloned hardware repo

--- src_QMS/hdw_dep/search_git.sh
+++ src_QMS/hdw_dep/search_git.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-# <Brainstorm portion>
-#REPOSITORIES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# REPOSITORIES=`pwd`
-#
-# IFS=$'\n'
-#
-# for REPO in `ls "$REPOSITORIES/"`
-# do
-#   if [ -d "$REPOSITORIES/$REPO" ]
-#   then
-#     echo "Updating $REPOSITORIES/$REPO at `date`"
-#     if [ -d "$REPOSITORIES/$REPO/.git" ]
-#     then
-#       cd "$REPOSITORIES/$REPO"
-#       git status
-#       echo "Fetching"
-#       git fetch
-#       echo "Pulling"
-#       git pull
-#     else
-#       echo "Skipping because it doesn't look like it has a .git folder."
-#     fi
-#     echo "Done at `date`"
-#     echo
-#   fi
-# done
-# <END Brainstorm portion/>
-
-
 #####################################################################
 #####################################################################
 # Hardware & Dependency Fetch
@@ -38,31 +9,61 @@
 #####################################################################
 #####################################################################
 
-
-
-# Verify git installed
-echo -e "\aChecking if git is installed."
-echo -e "\aWill install git if not detected."
-
 # Confirm to proceed
-if whiptail --yesno "Begin check and install of git?" 20 60 ; then
-# Prepare Basics
-sudo apt update -y
-sudo apt upgrade -y
-  # Check if git command exists
-  if ! command -v git &> /dev/null; then
-  echo "COMMAND could not be found"
-  # git command not found-->Installing git
-  sudo apt install git -y
-  elif  command -v git &> /dev/null; then
-  # git command found-->Continue to fetch
-  echo "COMMAND found!"
-  fi
+if whiptail --yesno "Check/Install git and download repository?" 20 60; then
+	# Prepare Basics
+	sudo apt update -y
+	sudo apt upgrade -y
+	# Check if git command exists
+	if ! command -v git &>/dev/null; then
+		echo "COMMAND could not be found"
+		# git command not found-->Installing git
+		sudo apt install git -y
+	elif command -v git &>/dev/null; then
+		# git command found-->Continue to fetch
+		echo "COMMAND found!"
+	fi
 else
-echo "Process declined, now exiting.."
-exit 1
+	echo "Process declined, now exiting.."
+	exit 1
 fi
-
 
 # Main Fetch Zone
 # Scan GitHub: FIUSCIS-CDA for hardware / dependencies
+
+# Describe Actions to user
+echo "From FIUSCIS-CDA GitHub: Type name of hardware repositories to clone."
+echo "Required dependencies for hardware will also be installed."
+
+# Collect names FIXME to multiple varianbles later
+read -p "Enter exact name of hardware repository: \n > " hardware_repo
+
+# Clone desired repository FIXME to multiple repositories later
+git clone "https://github.com/FIUSCIS-CDA/$hardware_repo.git"
+
+## Below is a custom solution to extract strings from QSF file to be
+## used as variables in git clone command to download required dependencies
+## FIXME if a better way is found
+
+# Move to newly downloaded directory
+cd "$hardware_repo/"
+
+# Initial delimitter to output string in third index when / is encountered
+awk -F/ '{print $3}' "$hardware_repo.qsf" >tmp.txt
+
+# Clean tmp.txt of excess newlines and remove strings that don't end in .bdf
+grep -e ".*\.bdf" tmp.txt >tmp1.txt
+
+# Final pass through tmp.txt to to trim .bdf extension from lines
+sed 's/.bdf//g' tmp1.txt >tmp.txt
+
+# Read tmp.txt using each line as a parameter in git clone
+while IFS= read -r dependencies; do
+	echo "List of dependencies required: $dependencies"
+	# FIXME add user confirmation to proceed and warning if no
+	# Clone necessary dependencies
+	git clone "https://github.com/FIUSCIS-CDA/$dependencies.git"
+done <tmp.txt
+
+## TODO: Go over each cloned hardware dependency to download their dependencies
+## Thinking of organizing each task into seperate functions to be called recursively 
